0b7ef7437eb4f0e08e93ed93ccbf564c0f438fd6
Inivinska] <nimsara.pasindu@inivosglobal.com>
2024-12-11 16:03:24 +0530

    init

---
+++ src/main/java/com/example/web_socket_basic/WebSocketBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_socket_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_socket_basic/WebSocketBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_socket_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
